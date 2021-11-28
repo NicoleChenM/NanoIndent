170c7b55f541f636bb603b1f8c905200b25355d4
NicoleChenM <92823883+NicoleChenM@users.noreply.github.com>
2021-11-28 10:51:44 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ MOCK_MODULES = ['numpy', 'math', 'io', 'lmfit', 're', 'os', 'traceback', 'pandas
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
-sys.path.insert(0, os.path.abspath('../../nanoIndent'))
+sys.path.insert(0, os.path.abspath('../../simpleble/'))
 
 
 # -- Project information -----------------------------------------------------
