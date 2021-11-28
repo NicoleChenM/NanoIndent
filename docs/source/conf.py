7feab7145f262f94d6731cd90f160e48100fd6d5
NicoleChenM <92823883+NicoleChenM@users.noreply.github.com>
2021-11-28 09:57:23 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,10 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.doctest']
+              'sphinx.ext.doctest',
+              'autoapi.extension',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages']
 
 
 # Add any paths that contain templates here, relative to this directory.
